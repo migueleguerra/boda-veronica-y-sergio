961e81219893db8a02285d5e377754ea9c361501
migueleguerra <guetop88@gmail.com>
2024-06-24 22:50:16 -0600

    Update font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ export const Hero = () => {
   return (
     <header className="col-[full-start/full-end] flex justify-center items-center h-screen">
       <div className="flex flex-col items-center sm:space-y-3 space-y-1 absolute md:-translate-y-[8rem] -translate-y-[10rem] z-10">
-        <h1 className="md:text-5xl text-4xl mb-5 font-[800]">
-          Verónica y Juan Sergio
+        <h1 className="md:text-4xl table-caption text-2xl mb-5 font-[800]">
+          VERÓNICA Y JUAN SERGIO
         </h1>
-        <h2 className="text-2xl font-[800]">Comming Soon...</h2>
+        <h2 className="text-xl font-[800]">Comming Soon...</h2>
         {/* <h3 className="text-xl font-[500]">04 Mayo 2024</h3>
         <h3 className="text-xl font-[500]">Oaxaca, Oaxaca</h3> */}
       </div>
